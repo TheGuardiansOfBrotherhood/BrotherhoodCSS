b3d9185a26d77e90f25ec6d8c4efefea797b588a
jacksoncastro <jack.vasc@yahoo.com.br>
2017-10-22 18:32:23 -0300

    Create gulp script

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+var clean = require('gulp-clean');
+var rename = require('gulp-rename');
+
+gulp.task('default', ['clean', 'clean-css', 'copy-fonts']);
+
+gulp.task('clean', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('clean-css', () => {
+	return gulp.src('main.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-fonts', () => {
+	gulp.src('fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+});
